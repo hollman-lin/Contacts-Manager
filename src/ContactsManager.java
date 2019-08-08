00e957751e3ee9435468b9378bb06880279a9eb7
Peter Hollman <peterjhollman@gmail.com>
2019-08-08 16:16:17 -0500

    added search and worked on delete function

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -88,11 +88,44 @@ public class ContactsManager {
         } else if (userInput.equals("3")){
             System.out.println("What would you like to search for?");
             Scanner scanner = new Scanner(System.in);
-            String searchInput = scanner.nextLine();
-            System.out.println();
-            parseFile("contacts.txt", searchInput);
+            String searchInput = scanner.nextLine().toLowerCase();
+            File file = new File("contacts.txt");
+            Scanner in = null;
+            try {
+                in = new Scanner(file);
+                while(in.hasNextLine())
+                {
+                    String line=in.nextLine().toLowerCase();
+                    if(line.contains(searchInput))
+                        System.out.println(line);
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         } else if (userInput.equals("4")){
-            System.out.println("delete something");
+            System.out.println("What would you like to delete");
+            Scanner deleteScanner = new Scanner(System.in);
+            String deleteInput = deleteScanner.nextLine().toLowerCase();
+            try{
+
+            String charset = "UTF-8";
+            File file = new File("contacts.txt");
+            File temp = File.createTempFile("file", ".txt", file.getParentFile());
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), charset));
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(new FileOutputStream(temp), charset));
+            for (String line; (line = reader.readLine()) != null;) {
+                line = line.replace(deleteInput, "");
+                writer.println(line);
+            }
+            reader.close();
+            writer.close();
+            file.delete();
+            temp.renameTo(file);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
         } else if (userInput.equals("5")){
             keepGoing=false;
         }else{
